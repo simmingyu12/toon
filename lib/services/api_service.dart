142113b1358a8db4e479854796adf4564a529f2c
simmingyu <smmin1212@naver.com>
2023-06-02 15:35:39 +0900

    PROJECT INIT

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,9 +1,23 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:toon/models/webtoon_model.dart';
+
+
 class ApiService {
   final String basUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
   final String today = "today";
 
-  void getTodaysToons(){
-
+  void getTodaysToons()async{
+    final Url = Uri.parse('$basUrl/$today');
+    final response = await http.get(Url);
+    if(response.statusCode == 200) {
+      final List<dynamic> webtoons = jsonDecode(response.body);
+      for (var webtoon in webtoons) {
+        WebtoonModel.fromJson(webtoon);
+      }
+      return;
+    }
+    throw Error();
   }
-
 }
