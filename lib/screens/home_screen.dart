8976634486ff30f6c2796c9fba3a5a6e88188713
simmingyu <smmin1212@naver.com>
2023-06-06 13:27:13 +0900

    PROJECT INIT

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:toon/models/webtoon_model.dart';
 import 'package:toon/services/api_service.dart';
+import 'package:toon/widgets/webtoon_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
@@ -12,20 +13,24 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        elevation: 3,
+        elevation: 2,
         backgroundColor: Colors.white,
-        foregroundColor: Colors.greenAccent,
-        title: const Text("오늘의 웹툰",style: TextStyle(fontSize: 30, height: 1.0),
+        foregroundColor: Colors.green,
+        title: const Text(
+          "어늘의 웹툰",
+          style: TextStyle(
+            fontSize: 24,
+          ),
         ),
       ),
       body: FutureBuilder(
         future: webtoons,
-        builder: (context, snapshot){
-          if(snapshot.hasData){
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
             return Column(
               children: [
                 const SizedBox(
-                  height: 20,
+                  height: 50,
                 ),
                 Expanded(child: makeList(snapshot))
               ],
@@ -39,46 +44,20 @@ class HomeScreen extends StatelessWidget {
     );
   }
 
-  ListView makeList(AsyncSnapshot<List<dynamic>> snapshot) {
+  ListView makeList(AsyncSnapshot<List<WebtoonModel>> snapshot) {
     return ListView.separated(
       scrollDirection: Axis.horizontal,
       itemCount: snapshot.data!.length,
-      padding: const EdgeInsets.symmetric(vertical: 10,horizontal:20 ),
-      itemBuilder: (context, index){
-        print(index);
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      itemBuilder: (context, index) {
         var webtoon = snapshot.data![index];
-        return Column(
-          children: [
-            Container(
-              width: 250,
-              clipBehavior: Clip.hardEdge,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                boxShadow: [
-                  BoxShadow(
-                    blurRadius: 15,
-                    offset: Offset(10,1),
-                    color: Colors.black.withOpacity(0.5)
-                  )
-                ]
-              ),
-              child: Image.network(webtoon.thumb),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Text(webtoon.title,
-              style: const TextStyle(
-                fontSize: 22,
-              ),
-            ),
-          ],
+        return Webtoon(
+          title: webtoon.title,
+          thumb: webtoon.thumb,
+          id: webtoon.id,
         );
       },
-      separatorBuilder: (context, index) => const SizedBox(width: 20,),
+      separatorBuilder: (context, index) => const SizedBox(width: 40),
     );
   }
 }
-
-
-
